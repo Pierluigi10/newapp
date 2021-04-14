213be1a39616d74865ef3525dc9ce2f7a496648f
pierluigi <pierluigibaiano@gmail.com>
2021-04-14 17:31:08 +0200

    props e map

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from "react";
 import "./App.css";
 import Test from "./components/test";
 // import image from "./data/image.json";
+import dataTests from "./data/datatest.json";
 
 function App() {
   return (
-    <div className="App">
-      <Test title="Forza Napoli" />
-      <Test title="Forza Napoli" />
-      <Test title="Forza Napoli" />
+    <div className='App'>
+           {dataTests.map((dataTest) => {
+        return (
+          <div>
+            <Test title={dataTest.title} paragraph={dataTest.paragraph} imagePath={dataTest.imagePath} paragraph2={dataTest.paragraph2} />
+            {/* <Test title={dataTest.title} imagePath={dataTest.imagePath} />
+            <Test title={dataTest.title} paragraph={dataTest.paragraph} /> */}
+          </div>
+        );
+      })}
     </div>
   );
 }
-
 export default App;
--- src/components/test.js
+++ src/components/test.js
@@ -1,12 +1,14 @@
 import React from "react";
-import image from "../data/image.json";
+// import image from "../data/image.json";
+
 
 function Test(props) {
   return (
     <div className="test">
       <h1>{props.title}</h1>
-      <img className="giacomo" src={image.image2} alt="Giacomo Capoccia" />
+      <img className="giacomo" src={props.imagePath} alt="Giacomo Capoccia" />
       <p>{props.paragraph}</p>
+      <p>{props.paragraph2}</p>
     </div>
   );
 }
